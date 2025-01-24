97cc5a834e167d5a2ff36a4fb6654b14fed6a440
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-24 20:47:40 +0600

    8.2 Optimizing Database Query.

--- task-mangement/tasks/views.py
+++ task-mangement/tasks/views.py
@@ -15,20 +15,39 @@ from django.db.models import Q, Count, Max, Min, Avg, Sum
 #     return HttpResponse("Welcome to the task management system")
 
 def manager_dashboard(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.select_related('details').prefetch_related('assigned_to').all()
 
-    # getting task count 
-    total_task = tasks.count()
-    completed_task = Task.objects.filter(status = "COMPLETED").count()
-    in_progress_task = Task.objects.filter(status = "IN_PROGRESS").count()
-    pending_task = Task.objects.filter(status = "PENDING").count()
+    ''' getting task count ''' 
+    # total_task = tasks.count()
+    # completed_task = Task.objects.filter(status = "COMPLETED").count()
+    # in_progress_task = Task.objects.filter(status = "IN_PROGRESS").count()
+    # pending_task = Task.objects.filter(status = "PENDING").count()
+
+    # count = {
+    #     'total_task': ,
+    #     'completed_task': ,
+    #     'in_progress_task': ,
+    #     'pending_task': ,
+    # }
+
+    # context = {
+    #     'tasks': tasks,
+    #     'total_task': total_task,
+    #     'completed_task': completed_task,
+    #     'in_progress_task': in_progress_task,
+    #     'pending_task': pending_task,
+    # }
+
+    counts = Task.objects.aggregate(
+        total = Count('id'),
+        completed = Count('id', filter = Q(status = 'COMPLETED')),
+        in_progress = Count('id', filter = Q(status = 'IN_PROGRESS')),
+        pending = Count('id', filter = Q(status = 'PENDING')),
+    )
 
     context = {
         'tasks': tasks,
-        'total_task': total_task,
-        'completed_task': completed_task,
-        'in_progress_task': in_progress_task,
-        'pending_task': pending_task,
+        'counts': counts,
     }
 
     return render(request, "dashboard/manager-dashboard.html", context)
