58d7df47198ca5e048d253737e8466ee80d30ffb
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-25 02:43:44 +0600

    8.5 Update Query on Database

--- task-mangement/tasks/views.py
+++ task-mangement/tasks/views.py
@@ -77,6 +77,31 @@ def create_task(request):
     context = { "task_form": task_form, "task_detail_form": task_detail_form }
     return render(request, "task_form.html", context)
 
+
+def update_task(request, id):
+    task = Task.objects.get(id=id)
+    # employees = Employee.objects.all()
+    task_form = TaskModelForm(instance=task) # For GET. By default GET method thake, tay GET bola lagbe na
+
+    if task.details:
+        task_detail_form = TaskDetailModelForm(instance=task.details)
+
+    if request.method == "POST": # For POST method.
+        task_form = TaskModelForm(request.POST, instance=task)
+        task_detail_form = TaskDetailModelForm(request.POST, instance=task.details)
+        if task_form.is_valid() and task_detail_form.is_valid():
+            """ For Model Form Data """
+            task = task_form.save()
+            task_detail = task_detail_form.save(commit=False)
+            task_detail.task = task
+            task_detail.save()
+
+            messages.success(request, "Task Updated Successfully")
+            return redirect('update-task', id)
+        
+    context = { "task_form": task_form, "task_detail_form": task_detail_form }
+    return render(request, "task_form.html", context)
+
 # def view_task(request):
 #     # retrive all data from tasks model 
 #     tasks = Task.objects.all()
