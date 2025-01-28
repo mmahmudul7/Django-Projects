8e7bcd243be33c735a3e3df4d06b3fd2ef5e892b
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-28 17:00:14 +0600

    10.2 Signup Using User Creation Form

--- task-mangement/task_management/urls.py
+++ task-mangement/task_management/urls.py
@@ -5,4 +5,5 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
+    path('users/', include('users.urls')),
 ] + debug_toolbar_urls()
