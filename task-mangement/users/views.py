53f2fbd070941cb7d1575dcbf4cb92964e7e967c
mmahmudul7 <hasanitgroup@gmail.com>
2025-01-30 20:41:00 +0600

    11.7 Activate User

--- task-mangement/users/views.py
+++ task-mangement/users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from users.forms import CustomRegistrationsForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from users.forms import LoginForm
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.models import User
 
 # Create your views here.
 def sign_up(request):
@@ -44,3 +46,15 @@ def sign_out(request):
         # messages.success(request, "You have been logged out successfully.")
         return redirect('sign-in')
     
+
+def activate_user(request, user_id, token):
+    try:
+        user = User.objects.get(id=user_id)
+        if default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return redirect('sign-in')
+        else:
+            return HttpResponse('The link has expired!')
+    except User.DoesNotExist:
+        return HttpResponse('User not found.')
