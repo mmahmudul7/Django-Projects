95b5ace31536300aee218b5c5bf8353587fcad88
mmahmudul7 <hasanitgroup@gmail.com>
2025-02-18 21:45:12 +0600

    16.3 ProfileView using TemplateView

--- task-mangement/users/urls.py
+++ task-mangement/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from users.views import sign_up, sign_in, sign_out, activate_user, admin_dashboard, assign_role, create_group, group_list, CustomLoginView
+# from users.views import sign_up, sign_in, sign_out, activate_user, admin_dashboard, assign_role, create_group, group_list, CustomLoginView, ProfileView
+from users.views import sign_up, sign_out, activate_user, admin_dashboard, assign_role, create_group, group_list, CustomLoginView, ProfileView
 
 
 urlpatterns = [
@@ -12,5 +13,6 @@ urlpatterns = [
     path('admin/dashboard/', admin_dashboard, name='admin-dashboard'),
     path('admin/<int:user_id>/assign-role/', assign_role, name='assign-role'),
     path('admin/create-group/', create_group, name='create-group'),
-    path('admin/group-list/', group_list, name='group-list')
+    path('admin/group-list/', group_list, name='group-list'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- task-mangement/users/views.py
+++ task-mangement/users/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Prefetch
 from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
 
 
 # Create your views here.
@@ -126,6 +127,23 @@ def create_group(request):
 def group_list(request):
     groups = Group.objects.prefetch_related('permissions').all()
     return render(request, 'admin/group_list.html', {'groups': groups})
+
+
+class ProfileView(TemplateView):
+    template_name = 'accounts/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context['username'] = user.username
+        context['email'] = user.email
+        context['name'] = user.get_full_name()
+
+        context['member_since'] = user.date_joined
+        context['last_login'] = user.last_login
+
+        return context
 
 
 """ 
