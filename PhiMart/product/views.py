e95949456a68b9beebae13fab9848611dbd6027f
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-05 15:45:58 +0600

    20.2 Introduction to get_object_or_404

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,16 +8,14 @@ from product.models import Product
 
 @api_view()
 def view_specific_product(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        product_dict = {
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-        }
-        return Response(product_dict)
-    except Product.DoesNotExist:
-        return Response({'message': 'Product dones not exists'}, status=status.HTTP_404_NOT_FOUND)
+    product = get_object_or_404(Product, pk = id)
+    product_dict = {
+        'id': product.id,
+        'name': product.name,
+        'price': product.price,
+    }
+    return Response(product_dict)
+
 
 
 @api_view()
