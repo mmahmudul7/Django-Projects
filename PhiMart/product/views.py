621b80e3879420fc7a0a6586bf00fa6b9cbbfb38
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-05 15:38:28 +0600

    20.2 product not found try except

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -1,11 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from product.models import Product
 
 # Create your views here.
 
 @api_view()
-def view_products(request):
-    return Response({"message": "Okay"})
+def view_specific_product(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        product_dict = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+        }
+        return Response(product_dict)
+    except Product.DoesNotExist:
+        return Response({'message': 'Product dones not exists'})
 
 
 @api_view()
