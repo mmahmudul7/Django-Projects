d3baa2e9a461acde975a7d60323e0881ba2cb150
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-05 16:31:17 +0600

    20.3 Use Django Serializer

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -3,19 +3,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from product.models import Product
+from product.serializers import ProductSerializer
+
 
 # Create your views here.
 
 @api_view()
 def view_specific_product(request, id):
     product = get_object_or_404(Product, pk = id)
-    product_dict = {
-        'id': product.id,
-        'name': product.name,
-        'price': product.price,
-    }
-    return Response(product_dict)
-
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
 
 @api_view()
