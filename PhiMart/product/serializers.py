48e00048622d62f768eda00c07cc8a7a72d9364d
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-06 15:36:42 +0600

    20.7 Object Lavel Validation

--- PhiMart/product/serializers.py
+++ PhiMart/product/serializers.py
@@ -63,8 +63,13 @@ class ProductSerializer(serializers.ModelSerializer):
     def calculate_tax(self, product):
         return round(product.price * Decimal(1.1), 2)
     
-
+    # Field Level Validation 
     def validate_price(self, price):
         if price < 0:
             raise serializers.ValidationError('Price could not be negative')
         return price
+    
+    # Object Level Validation 
+    # def validate(self, attrs):
+    #     if attrs['password1'] != attrs['password2']:
+    #         raise serializers.ValidationError("Password didn't match!")
