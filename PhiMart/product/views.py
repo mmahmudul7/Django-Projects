08c50b08aa6c7d907ef06b66b4dd3c5dd7eafffa
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-07 11:09:01 +0600

    21.3 Mixins & Generic API View

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -6,22 +6,10 @@ from product.serializers import ProductSerializer, CategorySerializer
 from django.db.models import Count
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.generics import ListCreateAPIView
 
 # Create your views here.
-
-@api_view(['GET', 'POST'])
-def view_products(request):
-    if request.method == 'GET':
-        products = Product.objects.select_related('category').all()
-        serializer = ProductSerializer(products, many = True)
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = ProductSerializer(data=request.data) # Deserializer
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
 
 class ViewProducts(APIView):
     def get(self, request):
@@ -36,32 +24,19 @@ class ViewProducts(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-# @api_view(['GET', 'UPDATE', 'DELETE'])
-# @api_view(['GET', 'PUT/PATCH', 'DELETE'])
-# PUT - Sends data that updates the entire resource 
-# PATCH - Sends partial data that updates specific fields 
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related('category').all()
+    serializer_class = ProductSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def view_specific_product(request, id):
-    if request.method == 'GET':
-        product = get_object_or_404(Product, pk = id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+    # def get_queryset(self):
+    #     return Product.objects.select_related('category').all()
     
-    if request.method == 'PUT':
-        product = get_object_or_404(Product, pk = id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    # def get_serializer_class(self):
+    #     return ProductSerializer
     
-    if request.method == 'DELETE':
-        product = get_object_or_404(Product, pk = id)
-        copy_of_product = product
-        product.delete()
-        serializer = ProductSerializer(copy_of_product)
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-    
+    # def get_serializer_context(self):
+    #     return {'request': self.request}
+
 
 class ViewSpecificProduct(APIView):
     def get(self, request, id):
@@ -84,23 +59,6 @@ class ViewSpecificProduct(APIView):
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def view_categories(request):
-    if request.method == 'GET':
-        categories = Category.objects.annotate(product_count=Count('products')).all()
-        serializer = CategorySerializer(categories, many = True)
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = CategorySerializer(data=request.data) # Deserializer
-        if serializer.is_valid():
-            print(serializer.validated_data)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-
 class ViewCategories(APIView):
     def get(self, request):
         categories = Category.objects.annotate(product_count=Count('products')).all()
@@ -112,15 +70,6 @@ class ViewCategories(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # else:
-        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view()
-def view_specific_category(request, pk):
-    category = get_object_or_404(Category, pk = pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
 
 
 class ViewSpecificCategory(APIView):
