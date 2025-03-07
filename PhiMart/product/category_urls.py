87eede45479c92646d267b9ac708664d2bfded90
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-07 15:09:42 +0600

    cleaning

--- PhiMart/product/category_urls.py
+++ PhiMart/product/category_urls.py
@@ -3,6 +3,5 @@ from product import views
 
 urlpatterns = [
     path('', views.ProductCategories.as_view(), name='category-list'),
-    # path('<int:id>/', views.ViewSpecificCategory.as_view(), name='view-specific-category'),
     path('<int:pk>/', views.CategoryDetails.as_view(), name='view-specific-category'),
 ]
--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -29,27 +29,6 @@ class ProductDetails(RetrieveUpdateDestroyAPIView):
     #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ViewSpecificProduct(APIView):
-    def get(self, request, id):
-        product = get_object_or_404(Product, pk = id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        product = get_object_or_404(Product, pk = id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, id):
-        product = get_object_or_404(Product, pk = id)
-        copy_of_product = product
-        product.delete()
-        serializer = ProductSerializer(copy_of_product)
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-
-
 class ProductCategories(ListCreateAPIView):
     queryset = Category.objects.annotate(product_count=Count('products')).all()
     serializer_class = CategorySerializer
@@ -58,37 +37,3 @@ class ProductCategories(ListCreateAPIView):
 class CategoryDetails(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.annotate(product_count=Count('products')).all()
     serializer_class = CategorySerializer
-
-
-class ViewSpecificCategory(APIView):
-    def get(self, request, id):
-        category = get_object_or_404(
-            Category.objects.annotate(
-                product_count = Count('products')
-            ).all(),
-            pk = id
-        )
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        category = get_object_or_404(
-            Category.objects.annotate(
-                product_count = Count('products')
-            ).all(),
-            pk = id
-        )
-        serializer = CategorySerializer(category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-    def delete(self, request, id):
-        category = get_object_or_404(
-            Category.objects.annotate(
-                product_count = Count('products')
-            ).all(),
-            pk = id
-        )
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
