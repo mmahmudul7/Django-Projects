8e4a665401a2f624a9f209d6ac45d0ee1a89f31b
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-07 11:40:04 +0600

    21.4 Customizing Generic Views

--- PhiMart/product/category_urls.py
+++ PhiMart/product/category_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    # path('', views.ViewCategories.as_view(), name='category-list'),
     path('', views.ProductCategories.as_view(), name='category-list'),
-    path('<int:id>/', views.ViewSpecificCategory.as_view(), name='view-specific-category'),
+    # path('<int:id>/', views.ViewSpecificCategory.as_view(), name='view-specific-category'),
+    path('<int:pk>/', views.CategoryDetails.as_view(), name='view-specific-category'),
 ]
--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -7,26 +7,26 @@ from django.db.models import Count
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
-
-class ViewProducts(APIView):
-    def get(self, request):
-        products = Product.objects.select_related('category').all()
-        serializer = ProductSerializer(products, many = True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data) # Deserializer
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 
 class ProductList(ListCreateAPIView):
     queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
+
+
+class ProductDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'id'
+
+    # def delete(self, request, pk):
+    #     product = get_object_or_404(Product, pk=pk)
+    #     if product.stock > 10:
+    #         return Response({'message': "Product with stock more than 10 could not be deleted"})
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ViewSpecificProduct(APIView):
@@ -50,20 +50,12 @@ class ViewSpecificProduct(APIView):
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
 
-class ViewCategories(APIView):
-    def get(self, request):
-        categories = Category.objects.annotate(product_count=Count('products')).all()
-        serializer = CategorySerializer(categories, many = True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data) # Deserializer
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
 class ProductCategories(ListCreateAPIView):
+    queryset = Category.objects.annotate(product_count=Count('products')).all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetails(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.annotate(product_count=Count('products')).all()
     serializer_class = CategorySerializer
 
