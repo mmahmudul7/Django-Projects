e3da44ae6fa86b5e85f8f4aecee94b9c5fd02b6e
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-12 01:24:38 +0600

    21.9 Pagination

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -6,7 +6,7 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from product.filters import ProductFilter
 from rest_framework.filters import SearchFilter, OrderingFilter
-# from rest_framework.pagination import PageNumberPagination
+from product.paginations import DefaultPagination
 
 
 class ProductViewSet(ModelViewSet):
@@ -14,7 +14,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
-    # pagination_class = PageNumberPagination
+    pagination_class = DefaultPagination
     search_fields = ['name', 'description']
     # search_fields = ['name', 'description', 'category__name']
     ordering_fields = ['price', 'updated_at']
