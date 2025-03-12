c32c71817cb1c78101db50a644be8bc58779d550
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-13 00:57:04 +0600

    22.1 Cart API Desing & Updating Models

--- PhiMart/order/models.py
+++ PhiMart/order/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from users.models import User
 from product.models import Product
+from uuid import uuid4
 # Create your models here.
 
+
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="cart")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Cart of {self.user.username}"
+        return f"Cart of {self.user.first_name}"
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(
@@ -17,8 +21,12 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
+    class Meta:
+        unique_together = [['cart', 'product']]
+
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
+
 
 class Order(models.Model):
     PENDING = 'Pending'
@@ -39,6 +47,7 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(
