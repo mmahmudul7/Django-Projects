01daa2659d7d1e7de8025e86f52be145e467b8a6
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-16 23:02:42 +0600

    22.5 Adding Cart Item

--- PhiMart/order/views.py
+++ PhiMart/order/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from order.models import Cart, CartItem
-from order.serializers import CartSerializer, CartItemSerializer
+from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
@@ -12,6 +12,19 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
 
 class CartItemViewSet(ModelViewSet):
     serializer_class = CartItemSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        return CartItemSerializer
+    
+    # def get_serializer(self):
+    #     return {'cart_id': self.kwargs['cart_pk']}
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs.setdefault('context', self.get_serializer_context())
+        kwargs['context']['cart_id'] = self.kwargs['cart_pk']
+        return super().get_serializer(*args, **kwargs)
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
