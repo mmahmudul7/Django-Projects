c5aaf4249fbe27f03467c6b3e9550f9777ed1166
mmahmudul7 <hasanitgroup@gmail.com>
2025-03-17 13:52:26 +0600

    23.2 Registering User API

--- PhiMart/phi_mart/settings.py
+++ PhiMart/phi_mart/settings.py
@@ -146,3 +146,9 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.UserCreateSerializer',
+    },
+}
