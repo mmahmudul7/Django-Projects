988476d90ab0e286d0abb145430b1127c86584e8
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-08 12:55:05 +0600

    28.7 Handling Events

--- phimart-client/src/components/List.jsx
+++ phimart-client/src/components/List.jsx
@@ -1,11 +1,27 @@
 const List = () => {
-    const fruits = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango', 'Pine Apple', 'Coconut'];
+    const fruits = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango', 'Pine Apple'];
+
+    let selectedIndex = -1;
+    const handleItemPrint = (index) => {
+        selectedIndex = index;
+        console.log(selectedIndex);
+    };
 
     return (
         <div>
             <ul className="pl-10 list-decimal text-xl">
-                {fruits.map((fruit) => {
-                    return <li key={fruit}>{fruit}</li>;
+                {fruits.map((fruit, index) => {
+                    return (
+                        <li
+                            className={
+                                selectedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""
+                            }
+                            onClick={() => handleItemPrint(index)}
+                            key={fruit}
+                        >
+                            {fruit}
+                        </li>
+                    );
                 })}
             </ul>
         </div>
