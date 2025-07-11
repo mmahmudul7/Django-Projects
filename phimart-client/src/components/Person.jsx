e538633660fc4cfc2bcd55e4a4ddb0f8ff3f42a4
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-11 06:56:49 +0600

    29.2 Updating State Object & Nested Object State

--- phimart-client/src/components/Person.jsx
+++ phimart-client/src/components/Person.jsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
 
 const Person = () => {
-    const [person, setPerson] = useState("")
-    // const [visible, setVisible] = useState(false)
+    const [person, setPerson] = useState({
+        firstName: "MD Mahmudul",
+        lastName: "Hasan",
+        email: "admin@onesix.dev",
+        address: {
+            city: "Kazla",
+            state: "Rajshahi",
+        },
+    });
 
     const handleClick = () => {
-        setPerson("Mahmud");
-        console.log(person);
+        const newPerson = {
+            ...person,
+            address: {
+                ...person.address,
+                state: "Dhaka",
+            },
+        };
+
+        setPerson(newPerson);
     };
-    console.log(person);
 
     return (
         <div className="m-5">
+            <h1>
+                {person.firstName} {person.lastName} {person.email}
+            </h1>
+            <p>
+                {person.address.city}, {person.address.state}
+            </p>
             <button
                 onClick={handleClick}
                 className='px-3 py-2 bg-blue-500 text-white rounded-sm'
