8ea70e0b9d5084df123d207057a0f9533653c10b
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-12 20:00:32 +0600

    Show Error Message by Axios

--- phimart-client/src/components/APIFetch.jsx
+++ phimart-client/src/components/APIFetch.jsx
@@ -3,21 +3,18 @@ import { useEffect, useState } from "react";
 
 const APIFetch = () => {
     const [users, setUsers] = useState([]);
-
-    // useEffect(() => {
-    //     fetch("https://jsonplaceholder.typicode.com/users")
-    //     .then((res) => res.json())
-    //     .then((data) => setUsers(data));
-    // }, []);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/users")
-        // .then((data) => console.log(data));
-        .then((data) => setUsers(data.data));
+        axios.get("https://jsonplaceholder.typicode.com/user")
+        .then((data) => setUsers(data.data))
+        // .catch((err) => console.log(err));
+        .catch((err) => setError(err.message));
     }, []);
 
     return (
         <div className="m-5">
+            {error && <p className="bg-red-100 text-red-900">{error}</p>}
             <ul className="m-5 list-disc">
                 {users.map((user) => (
                     <li key={user.id}>
