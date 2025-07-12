9406fdb2c510f8adbd62b507b59ba315c44301f1
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-12 08:11:56 +0600

    29.7 Understanding Effect Dependancies

--- phimart-client/src/components/Effect.jsx
+++ phimart-client/src/components/Effect.jsx
@@ -1,13 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Effect = () => {
+    const [users, setUsers] = useState([]);
+    const [dependancyA, setDependanceyA] = useState(0);
+    const [dependancyB, setDependanceyB] = useState(0);
+
     useEffect(() => {
         console.log("Effect occured");
-    });
+        
+        if (users.length === 0) {
+            setUsers(["Hasan"]);
+        }
+    }, [dependancyA, dependancyB]);
 
     return (
         <div>
-            <h1>UserList</h1>        
+            <h1>UserList</h1>
+            <button
+                onClick={() => setDependanceyA(Math.random())}
+                className='px-3 py-2 bg-blue-500 text-white me-2'
+            >
+                Click Me A
+            </button>        
+            <button
+                onClick={() => setDependanceyB(Math.random())}
+                className='px-3 py-2 bg-blue-500 text-white'
+            >
+                Click Me B
+            </button>        
         </div>
     );
 };
