8393091cf1865246a3696eff1e65147753d0d144
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-12 18:56:21 +0600

    Fetch Data Using Axios

--- phimart-client/src/components/APIFetch.jsx
+++ phimart-client/src/components/APIFetch.jsx
@@ -1,12 +1,19 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const APIFetch = () => {
     const [users, setUsers] = useState([]);
 
+    // useEffect(() => {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then((res) => res.json())
+    //     .then((data) => setUsers(data));
+    // }, []);
+
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((res) => res.json())
-        .then((data) => setUsers(data));
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        // .then((data) => console.log(data));
+        .then((data) => setUsers(data.data));
     }, []);
 
     return (
