c3215c6b9eb62e05cce7c4236c241dfbd753d2ca
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-14 02:09:14 +0600

    30.5 Form Validation

--- phimart-client/src/components/HookForm.jsx
+++ phimart-client/src/components/HookForm.jsx
@@ -1,7 +1,9 @@
 import { useForm } from "react-hook-form";
 
 const HookForm = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: {errors} } = useForm();
+    // console.log(formState);
+    // console.log(errors);
 
     const onSubmit = (data) => {
         console.log(data);
@@ -17,11 +19,13 @@ const HookForm = () => {
                         Name:
                     </label>
                     <input
-                        {...register("name")}
+                        {...register("name", { required: true, minLength: 5 })}
                         id = "name"
                         type="text"
                         className="w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md"
                     />
+                    {errors.name?.type == "required" && <span>This field is required</span>}
+                    {errors.name?.type == "minLength" && <span>Minimum length is 5</span>}
                 </div>
                 <div className="mt-3 mb-3">
                     <label className="block text-gray-700 text-sm font-bold mb-2"
@@ -30,11 +34,14 @@ const HookForm = () => {
                         Age:
                     </label>
                     <input
-                        {...register("age")}
+                        {...register("age", { required: true, min: 18, max: 60 })}
                         id = "age"
                         type="number"
                         className="w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md"
                     />
+                    {errors.age?.type == "required" && <span>This field is required</span>}
+                    {errors.age?.type == "min" && <span>Minimum age is 18</span>}
+                    {errors.age?.type == "max" && <span>Maximum age is 60</span>}
                 </div>
                 <button className="px-3 py-2 bg-blue-500 text-white rounded-md mt-2">
                     Submit
