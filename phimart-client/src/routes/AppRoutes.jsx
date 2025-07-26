e9a8b9592d909d510f963557f82ccfeb59f0a19b
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-26 21:16:17 +0600

    31.7 How to Get Design Ideas & Images

--- phimart-client/src/routes/AppRoutes.jsx
+++ phimart-client/src/routes/AppRoutes.jsx
@@ -1,19 +1,17 @@
 import { Route, Routes } from 'react-router';
-import Home from '../pages/Home/Home';
 import About from '../pages/About';
-import Product from '../pages/Product/Product';
 import MainLayout from '../layouts/MainLayout';
+import Home from '../pages/Home';
 
 const AppRoutes = () => {
     return (
         <Routes>
             {/* <Route index element={<Home/>}></Route>
             <Route path='about' element={<About />} /> */}
-            
+
             <Route element={<MainLayout />}>
-                <Route path='/' element={<Home />} />
-                <Route path='products' element={<Product />} />
-                <Route path='about' element={<About />} />
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
             </Route>
         </Routes>
     );
