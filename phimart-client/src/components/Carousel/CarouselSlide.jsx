0bce0f4e8a6fba1d10ec4a61f91a62f734142a46
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-27 22:09:20 +0600

    31.9 Creating Carousel

--- phimart-client/src/components/Carousel/CarouselSlide.jsx
+++ phimart-client/src/components/Carousel/CarouselSlide.jsx
@@ -1,19 +1,18 @@
-import book from '../../assets/images/book.png';
 import bgImg from '../../assets/images/banner-image-bg.webp';
 
-const CarouselSlide = () => {
+const CarouselSlide = ({ title, subtitle, image }) => {
     return (
-        <section className="w-full h-[650px]">
-            <div
-                className="max-w-6xl flex items-center justify-between px-8"
-                style={{ backgroundImage: `url(${bgImg})` }}
-            >
+        <section
+            className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-8"
+            style={{ backgroundImage: `url(${bgImg})` }}
+        >
+            <div className="max-w-6xl flex items-center justify-between px-8">
                 {/* Left Content  */}
                 <div className="w-1/2">
                     <h1 className="text-5xl font-bold text-gray-900">
-                        The Fine Print Book Collections
+                        {title}
                     </h1>
-                    <p className="text-gray-600 my-4">This is heading</p>
+                    <p className="text-gray-600 my-4">{subtitle}</p>
                     <button className="btn btn-secondary px-6 py-3 rounded-full shadow-md">
                         Shop Product
                     </button>
@@ -23,7 +22,7 @@ const CarouselSlide = () => {
                 <div className="w-1/2 flex justify-center">
                     <img
                         className="max-w-md drop-shadow-lg"
-                        src={book}
+                        src={image}
                         alt=""
                     />
                 </div>
