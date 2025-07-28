98e8a0707ac7a4e063b6aecd738458a8e5f3950d
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-28 06:13:44 +0600

    31.10 Making Carousel Responsive

--- phimart-client/src/components/Carousel/CarouselSlide.jsx
+++ phimart-client/src/components/Carousel/CarouselSlide.jsx
@@ -3,13 +3,13 @@ import bgImg from '../../assets/images/banner-image-bg.webp';
 const CarouselSlide = ({ title, subtitle, image }) => {
     return (
         <section
-            className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-8"
+            className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-4 md:px-8"
             style={{ backgroundImage: `url(${bgImg})` }}
         >
-            <div className="max-w-6xl flex items-center justify-between px-8">
+            <div className="max-w-6xl w-full flex flex-col md:flex-row items-center justify-between px-4 md:px-8">
                 {/* Left Content  */}
-                <div className="w-1/2">
-                    <h1 className="text-5xl font-bold text-gray-900">
+                <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0">
+                    <h1 className="text-4xl md:text-5xl font-bold text-gray-900">
                         {title}
                     </h1>
                     <p className="text-gray-600 my-4">{subtitle}</p>
@@ -19,9 +19,9 @@ const CarouselSlide = ({ title, subtitle, image }) => {
                 </div>
 
                 {/* Right Image  */}
-                <div className="w-1/2 flex justify-center">
+                <div className="w-full md:w-1/2 flex justify-center">
                     <img
-                        className="max-w-md drop-shadow-lg"
+                        className="max-w-full md:max-w-md drop-shadow-lg"
                         src={image}
                         alt=""
                     />
