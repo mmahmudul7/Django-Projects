53129d535517bc609a017a9745082fa2ebccc322
mmahmudul7 <hasanitgroup@gmail.com>
2025-07-31 20:45:43 +0600

    33.1 Creating Shop Page Product List

--- phimart-client/src/routes/AppRoutes.jsx
+++ phimart-client/src/routes/AppRoutes.jsx
@@ -2,16 +2,15 @@ import { Route, Routes } from 'react-router';
 import About from '../pages/About';
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/Home';
+import Shop from '../pages/Shop';
 
 const AppRoutes = () => {
     return (
         <Routes>
-            {/* <Route index element={<Home/>}></Route>
-            <Route path='about' element={<About />} /> */}
-
             <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="about" element={<About />} />
+                <Route path="shop" element={<Shop />} />
             </Route>
         </Routes>
     );
