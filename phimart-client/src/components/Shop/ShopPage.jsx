6464874c99803b550f98ef3cd6bad7c86f19d8be
mmahmudul7 <hasanitgroup@gmail.com>
2025-08-01 11:20:28 +0600

    33.3 Show Dynamic Product Using Pagination

--- phimart-client/src/components/Shop/ShopPage.jsx
+++ phimart-client/src/components/Shop/ShopPage.jsx
@@ -11,12 +11,13 @@ const ShopPage = () => {
 
     useEffect(() => {
         fetchProducts();
-    }, []);
+    }, [currentPage]);
 
+    /*
     const fetchProducts = () => {
         setLoading(true);
         apiClient
-            .get('/products')
+            .get(`/products/?page=${currentPage}`)
             .then((res) => {
                 setProducts(res.data.results);
                 setTotalPages(
@@ -25,6 +26,24 @@ const ShopPage = () => {
             })
             .catch((error) => console.log(error))
             .finally(() => setLoading(false));
+    };
+    */
+
+    const fetchProducts = async () => {
+        setLoading(true);
+        try {
+            const response = await apiClient.get(
+                `/products/?page=${currentPage}`
+            );
+            const data = await response.data;
+
+            setProducts(data.results);
+            setTotalPages(Math.ceil(data.count / data.results.length));
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
