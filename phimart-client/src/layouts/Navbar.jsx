3aa3581931c5e07fd38c01d10e2d356c4e4b40e2
mmahmudul7 <hasanitgroup@gmail.com>
2025-08-03 00:24:33 +0600

    fix profile image

--- phimart-client/src/layouts/Navbar.jsx
+++ phimart-client/src/layouts/Navbar.jsx
@@ -129,7 +129,7 @@ const Navbar = () => {
                         <div className="w-10 rounded-full">
                             <img
                                 alt="Tailwind CSS Navbar component"
-                                src="https://img.phiMart.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
                             />
                         </div>
                     </div>
