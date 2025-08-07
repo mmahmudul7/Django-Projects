86d4beafa9a690c2b36c63433e3e3b2db996f816
mmahmudul7 <hasanitgroup@gmail.com>
2025-08-07 20:21:09 +0600

    34.10 Handling Registration Error

--- phimart-client/src/hooks/useAuth.js
+++ phimart-client/src/hooks/useAuth.js
@@ -52,7 +52,18 @@ const useAuth = () => {
         try {
             await apiClient.post('/auth/users/', userData);
         } catch (error) {
-            console.log(error.response.data);
+            if (error.response && error.response.data) {
+                const errorMessage = Object.values(error.response.data)
+                    .flat()
+                    .join('\n');
+                setErrorMsg(errorMessage);
+                return { success: false, message: errorMessage };
+            }
+            setErrorMsg('Registration failed. Please try again.');
+            return {
+                success: false,
+                message: 'Registration failed. Please try again.',
+            };
         }
     };
 
