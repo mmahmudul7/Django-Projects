40230d056f66922b05e808ec66315f9ca9edeb32
mmahmudul7 <hasanitgroup@gmail.com>
2025-08-20 23:36:50 +0600

    37.5 Getting All Orders

--- phimart-client/src/pages/Orders.jsx
+++ phimart-client/src/pages/Orders.jsx
@@ -1,39 +1,13 @@
+import { useEffect, useState } from 'react';
 import OrderCard from '../components/Orders/OrderCard';
+import authApiClient from '../services/auth-api-client';
 
 const Orders = () => {
-    const orders = [
-        {
-            id: 'b941bd87-8bfa-4838-91c9-d11694826ab3',
-            user: 3,
-            status: 'Not Paid',
-            total_price: 600.0,
-            created_at: '2025-03-19T14:13:30.715441Z',
-            items: [
-                {
-                    id: 9,
-                    product: {
-                        id: 1,
-                        name: 'Smartphone',
-                        price: 150.0,
-                    },
-                    price: 150,
-                    quantity: 2,
-                    total_price: 300.0,
-                },
-                {
-                    id: 10,
-                    product: {
-                        id: 1,
-                        name: 'Tablet',
-                        price: 300.0,
-                    },
-                    price: 300.0,
-                    quantity: 1,
-                    total_price: 300.0,
-                },
-            ],
-        },
-    ];
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        authApiClient.get('/orders/').then((res) => setOrders(res.data));
+    }, []);
 
     return (
         <div className="container mx-auto px-4 py-8">
