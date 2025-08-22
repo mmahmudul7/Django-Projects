ae1ee66d295c0d5575fd7e322846b7e919cecf8d
mmahmudul7 <hasanitgroup@gmail.com>
2025-08-22 21:58:33 +0600

    37.8 Order Status Update By Admin

--- phimart-client/src/components/Orders/OrderCard.jsx
+++ phimart-client/src/components/Orders/OrderCard.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import useAuthContext from '../../hooks/useAuthContext';
 
 import OrderTable from './OrderTable';
+import authApiClient from '../../services/auth-api-client';
 
 const OrderCard = ({ order, onCancel }) => {
     const { user } = useAuthContext();
+    const [status, setStatus] = useState(order.status);
+
+    const handleStatusChange = async (event) => {
+        const newStatus = event.target.value;
+
+        try {
+            const response = await authApiClient.patch(
+                `/orders/${order.id}/update_status/`,
+                { status: newStatus }
+            );
+            console.log(response);
+            if (response.status === 200) {
+                setStatus(newStatus);
+                alert(response.data.status);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <div className="bg-white rounded-lg shadow-lg mb-8 overflow-hidden">
@@ -15,17 +36,55 @@ const OrderCard = ({ order, onCancel }) => {
                     </p>
                 </div>
                 <div className="flex gap-2">
-                    <span
-                        className={`px-3 py-1 rounded-full text-white text-sm font-medium ${
-                            order.status === 'Not Paid'
-                                ? 'bg-red-500'
-                                : 'bg-green-500'
-                        }`}
-                    >
-                        {order.status}
-                    </span>
+                    {user.is_staff ? (
+                        // <select
+                        //     value={status}
+                        //     onChange={handleStatusChange}
+                        //     className="px-3 py-1 rounded-full text-white text-sm font-medium bg-blue-500"
+                        // >
+                        //     <option value="Not Paid">Not Paid</option>
+                        //     <option value="Ready To Ship">Ready To Ship</option>
+                        //     <option value="Shipped">Shipped</option>
+                        //     <option value="Delivered">Delivered</option>
+                        //     <option value="Canceled">Canceled</option>
+                        // </select>
+                        <select
+                            value={status}
+                            onChange={handleStatusChange}
+                            className={`px-3 py-1 rounded-full text-white text-sm font-medium ${
+                                status === 'Not Paid'
+                                    ? 'bg-red-500'
+                                    : status === 'Ready To Ship'
+                                    ? 'bg-blue-500'
+                                    : status === 'Shipped'
+                                    ? 'bg-amber-500'
+                                    : status === 'Delivered'
+                                    ? 'bg-green-500'
+                                    : status === 'Canceled'
+                                    ? 'bg-gray-500'
+                                    : 'bg-blue-500'
+                            }`}
+                        >
+                            <option value="Not Paid">Not Paid</option>
+                            <option value="Ready To Ship">Ready To Ship</option>
+                            <option value="Shipped">Shipped</option>
+                            <option value="Delivered">Delivered</option>
+                            <option value="Canceled">Canceled</option>
+                        </select>
+                    ) : (
+                        <span
+                            className={`px-3 py-1 rounded-full text-white text-sm font-medium ${
+                                order.status === 'Not Paid'
+                                    ? 'bg-red-500'
+                                    : 'bg-green-500'
+                            }`}
+                        >
+                            {order.status}
+                        </span>
+                    )}
                     {order.status !== 'Deliverd' &&
-                        order.status !== 'Canceled' && (
+                        order.status !== 'Canceled' &&
+                        !user.is_staff && (
                             <button
                                 onClick={() => onCancel(order.id)}
                                 className="text-blue-700 hover:underline"
