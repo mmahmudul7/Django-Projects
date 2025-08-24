b1948dc4daa62f987524ae882838b7ab725d3908
mmahmudul7 <hasanitgroup@gmail.com>
2025-08-24 12:17:09 +0600

    38.4 Making Payment Information Dynamic

--- PhiMart/order/views.py
+++ PhiMart/order/views.py
@@ -102,32 +102,36 @@ class OrderViewSet(ModelViewSet):
 
 @api_view(['POST'])
 def initiate_payment(request):
+    user = request.user
+    amount = request.data.get("amount")
+    order_id = request.data.get("orderId")
+    num_items = request.data.get("numItems")
+
     settings = { 'store_id': 'onesi68a9f7161c754', 'store_pass': 'onesi68a9f7161c754@ssl', 'issandbox': True }
     sslcz = SSLCOMMERZ(settings)
     post_body = {}
-    post_body['total_amount'] = 100.26
+    post_body['total_amount'] = amount
     post_body['currency'] = "BDT"
-    post_body['tran_id'] = "12345"
-    post_body['success_url'] = "your success url"
-    post_body['fail_url'] = "your fail url"
-    post_body['cancel_url'] = "your cancel url"
+    post_body['tran_id'] = "txn_{order_id}"
+    post_body['success_url'] = "http://localhost:5173/dashboard/payment/success/"
+    post_body['fail_url'] = "http://localhost:5173/dashboard/payment/fail/"
+    post_body['cancel_url'] = "http://localhost:5173/dashboard/orders/"
     post_body['emi_option'] = 0
-    post_body['cus_name'] = "test"
-    post_body['cus_email'] = "test@test.com"
-    post_body['cus_phone'] = "01700000000"
-    post_body['cus_add1'] = "customer address"
+    post_body['cus_name'] = user.user.get_full_name()
+    post_body['cus_email'] = user.email
+    post_body['cus_phone'] = user.phone_number
+    post_body['cus_add1'] = user.address
     post_body['cus_city'] = "Dhaka"
     post_body['cus_country'] = "Bangladesh"
     post_body['shipping_method'] = "NO"
     post_body['multi_card_name'] = ""
-    post_body['num_of_item'] = 1
-    post_body['product_name'] = "Test"
-    post_body['product_category'] = "Test Category"
+    post_body['num_of_item'] = num_items
+    post_body['product_name'] = "E-commerce Products"
+    post_body['product_category'] = "General Category"
     post_body['product_profile'] = "general"
 
 
     response = sslcz.createSession(post_body) # API response
-    print(response)
     
     if response.get("status") == 'SUCCESS':
         return Response({"payment_url": response(['GatewayPageURL'])})
